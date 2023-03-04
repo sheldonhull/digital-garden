26f25e6e60055eb12162515c723af4234fabd5e4
Sheldon Hull <sheldonhull@users.noreply.github.com>
2023-03-04 15:04:46 -0600

    chore: fmt and trunk config
    feat: initial docs

--- magefile.go
+++ magefile.go
@@ -6,14 +6,47 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"strings"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"github.com/pterm/pterm"
 )
+
+const (
+	// portMapping is the port mapping for the mkdocs server running locally
+	portMapping = "3001:8000"
+
+	// mkdocsDockerImage is the docker image to use for running mkdocs
+	mkdocsDockerImage = "squidfunk/mkdocs-material"
+
+	// mkdocsDockerTag is the docker tag to use for running mkdocs
+	mkdocsDockerTag = "latest"
+)
+
+var (
+	// qualifiedDockerImage is the fully qualified docker image to use for running mkdocs
+	_qualifiedDockerImage = fmt.Sprintf("%s:%s", mkdocsDockerImage, mkdocsDockerTag)
+)
+
+// run the mkdocs server via docker
+func invokeMKDocs(args ...string) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	invokeArgs := []string{"run", "--rm", "-it", "-p", portMapping, "-v", fmt.Sprintf("%s:/docs", wd), _qualifiedDockerImage}
+	invokeArgs = append(invokeArgs, args...)
+	if mg.Verbose() {
+		invokeArgs = append(invokeArgs, "--verbose")
+	}
+	pterm.Success.Printfln("server on host via http://localhost:%s", strings.Split(portMapping, ":")[0])
+	return sh.RunV("docker", invokeArgs...)
+}
 
 // Run mkdocs server
 func Serve() error {
-	return sh.RunV("mkdocs", "serve")
+	return invokeMKDocs("serve")
 
 }
